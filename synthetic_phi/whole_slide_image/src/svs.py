38e03a43f9e13cb9b0275a660811e4becf368d8e
avirodov <Alexandr.Virodov@uky.edu>
2022-07-15 10:34:02 -0400

    fix bug where the file was not closed, and thus couldn't be moved if it doesn't actually contain identified information (early exit skips fp.close())

--- synthetic_phi/whole_slide_image/src/svs.py
+++ synthetic_phi/whole_slide_image/src/svs.py
@@ -20,103 +20,101 @@ def delete_associated_image(slide_path, image_type):
     if image_type not in allowed_image_types:
         raise Exception('Invalid image type requested for deletion')
 
-    fp = open(slide_path, 'r+b')
-    t = tifffile.TiffFile(fp)
+    with open(slide_path, 'r+b') as fp:
+        t = tifffile.TiffFile(fp)
 
-    # logic here will depend on file type. AT2 and older SVS files have "label" and "macro"
-    # strings in the page descriptions, which identifies the relevant pages to modify.
-    # in contrast, the GT450 scanner creates svs files which do not have this, but the label
-    # and macro images are always the last two pages and are striped, not tiled.
-    # The header of the first page will contain a description that indicates which file type it is
-    first_page = t.pages[0]
-    filtered_pages = []
-    if 'Aperio Image Library' in first_page.description:
-        filtered_pages = [page for page in t.pages if image_type in page.description]
-    elif 'Aperio Leica Biosystems GT450' in first_page.description:
-        if image_type == 'label':
-            filtered_pages = [t.pages[-2]]
+        # logic here will depend on file type. AT2 and older SVS files have "label" and "macro"
+        # strings in the page descriptions, which identifies the relevant pages to modify.
+        # in contrast, the GT450 scanner creates svs files which do not have this, but the label
+        # and macro images are always the last two pages and are striped, not tiled.
+        # The header of the first page will contain a description that indicates which file type it is
+        first_page = t.pages[0]
+        filtered_pages = []
+        if 'Aperio Image Library' in first_page.description:
+            filtered_pages = [page for page in t.pages if image_type in page.description]
+        elif 'Aperio Leica Biosystems GT450' in first_page.description:
+            if image_type == 'label':
+                filtered_pages = [t.pages[-2]]
+            else:
+                filtered_pages = [t.pages[-1]]
         else:
-            filtered_pages = [t.pages[-1]]
-    else:
-        # default to old-style labeled pages
-        filtered_pages = [page for page in t.pages if image_type in page.description]
+            # default to old-style labeled pages
+            filtered_pages = [page for page in t.pages if image_type in page.description]
 
-    num_results = len(filtered_pages)
-    if num_results > 1:
-        raise Exception(f'Invalid SVS format: duplicate associated {image_type} images found')
-    if num_results == 0:
-        # No image of this type in the WSI file; no need to delete it
-        return
+        num_results = len(filtered_pages)
+        if num_results > 1:
+            raise Exception(f'Invalid SVS format: duplicate associated {image_type} images found')
+        if num_results == 0:
+            # No image of this type in the WSI file; no need to delete it
+            return
 
-    # At this point, exactly 1 image has been identified to remove
-    page = filtered_pages[0]
+        # At this point, exactly 1 image has been identified to remove
+        page = filtered_pages[0]
 
-    # get the list of IFDs for the various pages
-    offsetformat = t.tiff.offsetformat
-    offsetsize = t.tiff.offsetsize
-    tagnoformat = t.tiff.tagnoformat
-    tagnosize = t.tiff.tagnosize
-    tagsize = t.tiff.tagsize
-    unpack = struct.unpack
+        # get the list of IFDs for the various pages
+        offsetformat = t.tiff.offsetformat
+        offsetsize = t.tiff.offsetsize
+        tagnoformat = t.tiff.tagnoformat
+        tagnosize = t.tiff.tagnosize
+        tagsize = t.tiff.tagsize
+        unpack = struct.unpack
 
-    # start by saving this page's IFD offset
-    ifds = [{'this': p.offset} for p in t.pages]
-    # now add the next page's location and offset to that pointer
-    for p in ifds:
-        # move to the start of this page
-        fp.seek(p['this'])
-        # read the number of tags in this page
-        (num_tags,) = unpack(tagnoformat, fp.read(tagnosize))
+        # start by saving this page's IFD offset
+        ifds = [{'this': p.offset} for p in t.pages]
+        # now add the next page's location and offset to that pointer
+        for p in ifds:
+            # move to the start of this page
+            fp.seek(p['this'])
+            # read the number of tags in this page
+            (num_tags,) = unpack(tagnoformat, fp.read(tagnosize))
 
-        # move forward past the tag defintions
-        fp.seek(num_tags * tagsize, 1)
-        # add the current location as the offset to the IFD of the next page
-        p['next_ifd_offset'] = fp.tell()
-        # read and save the value of the offset to the next page
-        (p['next_ifd_value'],) = unpack(offsetformat, fp.read(offsetsize))
+            # move forward past the tag defintions
+            fp.seek(num_tags * tagsize, 1)
+            # add the current location as the offset to the IFD of the next page
+            p['next_ifd_offset'] = fp.tell()
+            # read and save the value of the offset to the next page
+            (p['next_ifd_value'],) = unpack(offsetformat, fp.read(offsetsize))
 
-    # filter out the entry corresponding to the desired page to remove
-    pageifd = [i for i in ifds if i['this'] == page.offset][0]
-    # find the page pointing to this one in the IFD list
-    previfd = [i for i in ifds if i['next_ifd_value'] == page.offset]
-    # check for errors
-    if len(previfd) == 0:
-        raise Exception('No page points to this one')
-        return
-    else:
-        previfd = previfd[0]
+        # filter out the entry corresponding to the desired page to remove
+        pageifd = [i for i in ifds if i['this'] == page.offset][0]
+        # find the page pointing to this one in the IFD list
+        previfd = [i for i in ifds if i['next_ifd_value'] == page.offset]
+        # check for errors
+        if len(previfd) == 0:
+            raise Exception('No page points to this one')
+            return
+        else:
+            previfd = previfd[0]
 
-    # get the strip offsets and byte counts
-    offsets = page.tags['StripOffsets'].value
-    bytecounts = page.tags['StripByteCounts'].value
+        # get the strip offsets and byte counts
+        offsets = page.tags['StripOffsets'].value
+        bytecounts = page.tags['StripByteCounts'].value
 
-    # iterate over the strips and erase the data
-    # print('Deleting pixel data from image strips')
-    for (o, b) in zip(offsets, bytecounts):
-        fp.seek(o)
-        fp.write(b'\0' * b)
+        # iterate over the strips and erase the data
+        # print('Deleting pixel data from image strips')
+        for (o, b) in zip(offsets, bytecounts):
+            fp.seek(o)
+            fp.write(b'\0' * b)
 
-    # iterate over all tags and erase values if necessary
-    # print('Deleting tag values')
-    for key, tag in page.tags.items():
-        fp.seek(tag.valueoffset)
-        fp.write(b'\0' * tag.count)
+        # iterate over all tags and erase values if necessary
+        # print('Deleting tag values')
+        for key, tag in page.tags.items():
+            fp.seek(tag.valueoffset)
+            fp.write(b'\0' * tag.count)
 
-    offsetsize = t.tiff.offsetsize
-    offsetformat = t.tiff.offsetformat
-    pagebytes = (pageifd['next_ifd_offset'] - pageifd['this']) + offsetsize
+        offsetsize = t.tiff.offsetsize
+        offsetformat = t.tiff.offsetformat
+        pagebytes = (pageifd['next_ifd_offset'] - pageifd['this']) + offsetsize
 
-    # next, zero out the data in this page's header
-    # print('Deleting page header')
-    fp.seek(pageifd['this'])
-    fp.write(b'\0' * pagebytes)
+        # next, zero out the data in this page's header
+        # print('Deleting page header')
+        fp.seek(pageifd['this'])
+        fp.write(b'\0' * pagebytes)
 
-    # finally, point the previous page's IFD to this one's IFD instead
-    # this will make it not show up the next time the file is opened
-    fp.seek(previfd['next_ifd_offset'])
-    fp.write(struct.pack(offsetformat, pageifd['next_ifd_value']))
-
-    fp.close()
+        # finally, point the previous page's IFD to this one's IFD instead
+        # this will make it not show up the next time the file is opened
+        fp.seek(previfd['next_ifd_offset'])
+        fp.write(struct.pack(offsetformat, pageifd['next_ifd_value']))
 
 
 def deident_svs_file(original_file_path, deident_file_path):
